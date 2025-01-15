985d5fb29f1524b1869fdef28ad5741549abeac3
Abir HARRASSE <81148161+abirharrasse@users.noreply.github.com>
2025-01-15 17:23:09 +0100

    Update eap_graph.py

--- eap/eap_graph.py
+++ eap/eap_graph.py
@@ -198,156 +198,156 @@ class EAPGraph:
             downstream_hooks.sort(key=get_downstream_hook_level)
     
             return upstream_hooks, downstream_hooks
-    
-        def _get_hook_level(self, hook, component_ordering):
-            num_components_per_layer = len(component_ordering)
-            layer = int(hook.split(".")[1])
-            hook_type = hook.split(".")[-1]
-            component_order = component_ordering[hook_type]
-            return layer * num_components_per_layer + component_order
-    
-        def get_slice_previous_upstream_nodes(self, downstream_hook):
-            layer = downstream_hook.layer()
-            hook_type = downstream_hook.name.split(".")[-1]
-            
-            if hook_type == "hook_mlp_in":
-                return self.upstream_nodes_before_mlp_layer[layer]
-            elif hook_type in ["attn.hook_result", "hook_resid_post"]:
-                return self.upstream_nodes_before_layer[layer]
-    
-        def get_hook_slice(self, hook_name):
-            if hook_name in self.upstream_hook_slice:
-                return self.upstream_hook_slice[hook_name]
-            elif hook_name in self.downstream_hook_slice:
-                return self.downstream_hook_slice[hook_name]
-    
-        def reset_scores(self):
-            self.eap_scores = torch.zeros(
-                (self.n_upstream_nodes, self.n_downstream_nodes),
-                device=self.cfg.device
-            )
-    
-        def top_edges(self, n=1000, threshold=None, abs_scores=True):
-            assert self.eap_scores is not None, "EAP scores have not been computed yet"
-    
-            if abs_scores:
-                top_scores, top_indices = torch.topk(self.eap_scores.flatten().abs(), k=n, dim=0)
+
+    def _get_hook_level(self, hook, component_ordering):
+        num_components_per_layer = len(component_ordering)
+        layer = int(hook.split(".")[1])
+        hook_type = hook.split(".")[-1]
+        component_order = component_ordering[hook_type]
+        return layer * num_components_per_layer + component_order
+
+    def get_slice_previous_upstream_nodes(self, downstream_hook):
+        layer = downstream_hook.layer()
+        hook_type = downstream_hook.name.split(".")[-1]
+        
+        if hook_type == "hook_mlp_in":
+            return self.upstream_nodes_before_mlp_layer[layer]
+        elif hook_type in ["attn.hook_result", "hook_resid_post"]:
+            return self.upstream_nodes_before_layer[layer]
+
+    def get_hook_slice(self, hook_name):
+        if hook_name in self.upstream_hook_slice:
+            return self.upstream_hook_slice[hook_name]
+        elif hook_name in self.downstream_hook_slice:
+            return self.downstream_hook_slice[hook_name]
+
+    def reset_scores(self):
+        self.eap_scores = torch.zeros(
+            (self.n_upstream_nodes, self.n_downstream_nodes),
+            device=self.cfg.device
+        )
+
+    def top_edges(self, n=1000, threshold=None, abs_scores=True):
+        assert self.eap_scores is not None, "EAP scores have not been computed yet"
+
+        if abs_scores:
+            top_scores, top_indices = torch.topk(self.eap_scores.flatten().abs(), k=n, dim=0)
+        else:
+            top_scores, top_indices = torch.topk(self.eap_scores.flatten(), k=n, dim=0)
+
+        top_edges = []
+        for i, (abs_score, index) in enumerate(zip(top_scores, top_indices)):
+            if threshold is not None and abs_score < threshold:
+                break
+
+            upstream_node_idx, downstream_node_idx = np.unravel_index(index, self.eap_scores.shape)
+            score = self.eap_scores[upstream_node_idx, downstream_node_idx]
+
+            top_edges.append((self.upstream_nodes[upstream_node_idx], self.downstream_nodes[downstream_node_idx], score.item()))
+
+        return top_edges
+    def subgraph_top_edges(
+        self,
+        threshold=None,
+        abs_scores=True
+    ):
+        assert self.eap_scores is not None, "EAP scores have not been computed yet"
+
+        top_edges = self.top_edges(threshold=threshold, abs_scores=abs_scores)
+
+        upstream_nodes = [edge[0] for edge in top_edges]
+        downstream_nodes = [edge[1] for edge in top_edges]
+        subgraph = EAPGraph(upstream_nodes, downstream_nodes)
+
+        return subgraph
+
+    def show(
+        self,
+        threshold=None,
+        abs_scores=True,
+        fname: str="eap_graph.png"
+    ):
+        import pygraphviz as pgv
+
+        minimum_penwidth = 0.2
+        edges = self.top_edges(threshold=threshold, abs_scores=abs_scores)
+
+        g = pgv.AGraph(
+            name='root',
+            strict=True,
+            directed=True
+        )
+
+        g.graph_attr.update(ranksep='0.1', nodesep='0.1', compound=True)
+        g.node_attr.update(fixedsize='true', width='1.5', height='.5')
+
+        def find_layer_node(node):
+            if node == f'resid_post.{self.cfg.n_layers - 1}':
+                return self.cfg.n_layers
             else:
-                top_scores, top_indices = torch.topk(self.eap_scores.flatten(), k=n, dim=0)
-    
-            top_edges = []
-            for i, (abs_score, index) in enumerate(zip(top_scores, top_indices)):
-                if threshold is not None and abs_score < threshold:
-                    break
-    
-                upstream_node_idx, downstream_node_idx = np.unravel_index(index, self.eap_scores.shape)
-                score = self.eap_scores[upstream_node_idx, downstream_node_idx]
-    
-                top_edges.append((self.upstream_nodes[upstream_node_idx], self.downstream_nodes[downstream_node_idx], score.item()))
-    
-            return top_edges
-        def subgraph_top_edges(
-            self,
-            threshold=None,
-            abs_scores=True
-        ):
-            assert self.eap_scores is not None, "EAP scores have not been computed yet"
-    
-            top_edges = self.top_edges(threshold=threshold, abs_scores=abs_scores)
-    
-            upstream_nodes = [edge[0] for edge in top_edges]
-            downstream_nodes = [edge[1] for edge in top_edges]
-            subgraph = EAPGraph(upstream_nodes, downstream_nodes)
-    
-            return subgraph
-    
-        def show(
-            self,
-            threshold=None,
-            abs_scores=True,
-            fname: str="eap_graph.png"
-        ):
-            import pygraphviz as pgv
-    
-            minimum_penwidth = 0.2
-            edges = self.top_edges(threshold=threshold, abs_scores=abs_scores)
-    
-            g = pgv.AGraph(
-                name='root',
-                strict=True,
-                directed=True
-            )
-    
-            g.graph_attr.update(ranksep='0.1', nodesep='0.1', compound=True)
-            g.node_attr.update(fixedsize='true', width='1.5', height='.5')
-    
-            def find_layer_node(node):
-                if node == f'resid_post.{self.cfg.n_layers - 1}':
-                    return self.cfg.n_layers
-                else:
-                    return int(node.split(".")[1])
-    
-            layer_to_subgraph = {}
-            layer_to_subgraph[-1] = g.add_subgraph(name=f'cluster_-1', rank='same', color='invis')
-            layer_to_subgraph[-1].add_node(f'-1_invis', style='invis')
-    
-            min_layer = 999
-            max_layer = -1
-            layers = list(range(0, 32))
-    
-            for edge in edges:
-                parent_node = edge[0]
-                child_node = edge[1]
-                min_layer = min(min_layer, find_layer_node(parent_node))
-                max_layer = max(max_layer, find_layer_node(child_node))
-    
-            layers = list(range(min_layer, max_layer + 1))
-            prev_layer = None
-    
-            for layer in layers:
-                layer_to_subgraph[layer] = g.add_subgraph(name=f'cluster_{layer}', rank='same', color='invis')
-                layer_to_subgraph[layer].add_node(f'{layer}_invis', style='invis')
-    
-                if prev_layer is not None:
-                    g.add_edge(f'{prev_layer}_invis', f'{layer}_invis', style='invis', weight=1000)
-    
-                prev_layer = layer
-                    
-            # Adding nodes and edges between nodes
-            for edge in edges:
-                parent_node, child_node, edge_score = edge
-    
-                parent_name = parent_node
-                child_name = child_node
+                return int(node.split(".")[1])
+
+        layer_to_subgraph = {}
+        layer_to_subgraph[-1] = g.add_subgraph(name=f'cluster_-1', rank='same', color='invis')
+        layer_to_subgraph[-1].add_node(f'-1_invis', style='invis')
+
+        min_layer = 999
+        max_layer = -1
+        layers = list(range(0, 32))
+
+        for edge in edges:
+            parent_node = edge[0]
+            child_node = edge[1]
+            min_layer = min(min_layer, find_layer_node(parent_node))
+            max_layer = max(max_layer, find_layer_node(child_node))
+
+        layers = list(range(min_layer, max_layer + 1))
+        prev_layer = None
+
+        for layer in layers:
+            layer_to_subgraph[layer] = g.add_subgraph(name=f'cluster_{layer}', rank='same', color='invis')
+            layer_to_subgraph[layer].add_node(f'{layer}_invis', style='invis')
+
+            if prev_layer is not None:
+                g.add_edge(f'{prev_layer}_invis', f'{layer}_invis', style='invis', weight=1000)
+
+            prev_layer = layer
                 
-                for node_name in [parent_name, child_name]:
-                    node_layer = find_layer_node(node_name)
-                    node_color = '#1f77b4' if node_name.startswith("head") else '#ff7f0e' if node_name.startswith("mlp") else '#2ca02c' if node_name.startswith("resid") else '#d62728'
-    
-                    layer_to_subgraph[node_layer].add_node(
-                        node_name,
-                        fillcolor=node_color,
-                        color="black",
-                        style="filled, rounded",
-                        shape="box",
-                        fontname="Helvetica",
-                    )
-                    
-                edge_width = str(max(minimum_penwidth, edge_score*100))
-    
-                g.add_edge(
-                    parent_name,
-                    child_name,
-                    penwidth=edge_width,
-                    color='#0091E4',
-                    weight=10,
-                    minlen='0.5',
+        # Adding nodes and edges between nodes
+        for edge in edges:
+            parent_node, child_node, edge_score = edge
+
+            parent_name = parent_node
+            child_name = child_node
+            
+            for node_name in [parent_name, child_name]:
+                node_layer = find_layer_node(node_name)
+                node_color = '#1f77b4' if node_name.startswith("head") else '#ff7f0e' if node_name.startswith("mlp") else '#2ca02c' if node_name.startswith("resid") else '#d62728'
+
+                layer_to_subgraph[node_layer].add_node(
+                    node_name,
+                    fillcolor=node_color,
+                    color="black",
+                    style="filled, rounded",
+                    shape="box",
+                    fontname="Helvetica",
                 )
-    
-            save_path = os.path.join(DEFAULT_GRAPH_PLOT_DIR, fname)
-    
-            print(f"Saving graph")
-            if not fname.endswith(".gv"):
-                g.draw(path=save_path, prog='dot')
-    
-            return g
+                
+            edge_width = str(max(minimum_penwidth, edge_score*100))
+
+            g.add_edge(
+                parent_name,
+                child_name,
+                penwidth=edge_width,
+                color='#0091E4',
+                weight=10,
+                minlen='0.5',
+            )
+
+        save_path = os.path.join(DEFAULT_GRAPH_PLOT_DIR, fname)
+
+        print(f"Saving graph")
+        if not fname.endswith(".gv"):
+            g.draw(path=save_path, prog='dot')
+
+        return g
